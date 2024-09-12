bc573eea8dc19027058f599402a3807758573476
Jen Sheerin <84478520+jensheerin@users.noreply.github.com>
2024-09-12 17:02:10 -0400

    #62 verify support for version 4.0.0 (#63)
    * avm template updates
    
    * fix lint
    
    * provider update
    
    * v1,1,2
    
    * v1.1.2
    
    * 4.0.0 tested
    
    * fix lint
    
    * fix err
    
    * fix lint
    
    * fix lint
    
    * fix lint
    
    * fix lint
    
    * update constraints
    
    * fix typo
    
    * fix lint
    
    * fix lint

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.7.0, < 4.0.0"
+      version = ">= 3.71.0, < 5.0.0"
     }
     random = {
       source  = "hashicorp/random"
@@ -14,8 +14,6 @@ terraform {
 
 provider "azurerm" {
   features {}
-
-  subscription_id = var.subscription_id
 }
 
 
--- terraform.tf
+++ terraform.tf
@@ -3,12 +3,14 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.71.0, < 4.0.0"
+      version = ">= 3.71.0, < 5.0.0"
     }
+
     modtm = {
       source  = "azure/modtm"
       version = "~> 0.3"
     }
+
     random = {
       source  = "hashicorp/random"
       version = ">= 3.6.0, <4.0.0"
